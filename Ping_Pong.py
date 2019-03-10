5e44cebbf2fdfcc3d96eef1ffb57bcb8c5e6bd56
Aniket sangohi <36537460+asangohi@users.noreply.github.com>
2019-03-11 01:10:28 +0530

    Add files via upload

---
+++ Ping_Pong.py
@@ -0,0 +1,10 @@
+#importing the libraries required for the game
+import turtle
+import os
+
+#creating screen
+win = turtle.Screen()
+win.title("Ping Pong")
+win.bgcolor("black")
+win.setup(width=800, height=600)
+win.tracer(0)
