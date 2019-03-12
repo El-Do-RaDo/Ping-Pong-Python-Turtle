0f93177b74d742a9157ed52e0ae39c22f8cb9927
Aniket sangohi <36537460+asangohi@users.noreply.github.com>
2019-03-12 21:48:51 +0530

    Update Ping_Pong.py

--- Ping_Pong.py
+++ Ping_Pong.py
@@ -12,3 +12,13 @@ win.tracer(0)
 #scoring
 score_a = 0  
 score_b = 0
+
+#adding paddles and the ball to the screen
+#paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=0.5)
+paddle_a.penup()
+paddle_a.goto(-350,0)
