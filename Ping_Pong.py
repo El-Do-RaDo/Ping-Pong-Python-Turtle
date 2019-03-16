8e9f1f2ba422966e66bb86aa09642016a5e3aecb
Aniket sangohi <36537460+asangohi@users.noreply.github.com>
2019-03-17 01:08:20 +0530

    Update Ping_Pong.py

--- Ping_Pong.py
+++ Ping_Pong.py
@@ -41,3 +41,12 @@ ball.penup()
 ball.goto(0,0)
 ball.dx = 0.3
 ball.dy = 0.3
+
+#ccreating pen for scoring
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("green")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,280)
+pen.write("Player A: 0   Player B: 0", align="center",font=("couruer", 10, "normal"))
